8a510f1da9e5ba41c76098ddb6c82366a2905a90
pisit-wongthawin <pisitwongthawin1234@gmail.com>
2024-06-04 01:32:51 +0700

    make code work with FE

--- be/pisit/weather-app/src/index.js
+++ be/pisit/weather-app/src/index.js
@@ -1,43 +1,44 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT || 3215
+const PORT = process.env.PORT || 3215;
 
 // 200 success, 404 = not found, 400 bad request, 500 internal server error
 
-app.use(cors())
-app.get('/', async (request, response) => {
+app.use(cors());
+
+app.get("/", async (request, response) => {
+  let {
+    query: { city },
+  } = request;
+
+  if (!city) return response.status(400).json({ message: "Bad request" });
+
+  try {
+    const weatherResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1493ad0764f705adea83d06d072d49bb&units=metric&lang=th`
+    );
+
+    if (weatherResponse.status === 404)
+      return response.status(404).json({ message: "Not found" });
+
+    const fullWeatherData = await weatherResponse.json();
+
+    response.json({
+      description: fullWeatherData.weather[0].description,
+      temp: fullWeatherData.main.temp,
+      feels_like: fullWeatherData.main.feels_like,
+      humidity: fullWeatherData.main.humidity,
+    });
     
-    let {
-        query: {city}
-    } = request
-    if (!city)
-        return response.status(400).send('Bad request')
-
-    try{
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1493ad0764f705adea83d06d072d49bb&units=metric&lang=th`)
-    
-    if (data.status === 404)
-        response.status(404).send('Not found')
-
-    const result = await data.json()
-
-    let output = {}
-    output.description = result.weather[0].description
-    output.temp = result.main.temp
-    output.feels_like = result.main.feels_like
-    output.humidity = result.main.humidity
-
-    response.json(output)
-    } catch (e) {
-       console.error()  
-       response.status(500).send(e.name)
-    }
-    
-})
+  } catch (e) {
+    console.error();
+    return response.status(500).json({ message: e.name });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log('Running on Port', PORT)
-})
+  console.log("Running on Port", PORT);
+});
